e0e433d9f69a9ef234144feb0ef82f0eaab07b5b
Saran Hasnor <thomas.azoug@free.fr>
2015-10-21 12:37:03 +0200

    Added short documentation for some utility functions

--- Hasnor_Engine/engine_geometry.h
+++ Hasnor_Engine/engine_geometry.h
@@ -54,25 +54,25 @@ typedef struct {
 
 typedef struct {
 	
-	// Construction functions
+	/* Construction functions */
 	vertex_t*	(*addVertexToFace)(face_t *face, float x, float y, float z, float u, float v);
 	face_t*		(*addFaceToMesh)(mesh_t *mesh);
 	mesh_t*		(*newMesh)(void);
 
-	// Linking function
+	/* Linking function */
 	void		(*linkVertexToFace)(face_t *face, vertex_t *vertex);
 	void		(*linkFaceToMesh)(mesh_t *mesh, face_t *face);
 
-	// Quick construction functions (using selected objects)
+	/* Quick construction functions (using selected objects) */
 	vertex_t*	(*addVertex)(float x, float y, float z, float u, float v);
 	face_t*		(*addFace)(void);
 
-	// Destruction functions
+	/* Destruction functions */
 	void		(*destroyVertex)(vertex_t *vertex);
 	void		(*destroyFace)(face_t *face);
 	void		(*destroyMesh)(mesh_t *mesh);
 
-	// Transformation functions
+	/* Transformation functions */
 	void		(*moveVertex)(vertex_t *vertex, float x, float y, float z);
 	void		(*moveFace)(face_t *face, float x, float y, float z);
 	void		(*moveMesh)(mesh_t *mesh, float x, float y, float z);
@@ -83,33 +83,33 @@ typedef struct {
 	//void		(*scaleFace)(face_t *face, float x, float y, float z);
 	//void		(*scaleMesh)(mesh_t *mesh, float x, float y, float z);
 
-	// Duplication functions (unsafe for now)
+	/* Duplication functions (unsafe for now) */
 	vertex_t*	(*duplicateVertex)(vertex_t *vertex);
 	face_t*		(*duplicateFace)(face_t *face);
 	mesh_t*		(*duplicateMesh)(mesh_t *mesh);
 
-	// Selection management functions
+	/* Selection management functions */
 	void		(*setAutoSelect)(bool active);
 	void		(*selectFace)(face_t *face);
 	void		(*selectMesh)(mesh_t *mesh);
 	face_t*		(*getSelectedFace)(void);
 	mesh_t*		(*getSelectedMesh)(void);
 
-	// Cursor management functions
+	/* Cursor management functions */
 	void		(*setCursorPos)(float x, float y, float z);
 	void		(*setCursorCoords)(coords_t coords);
 	void		(*moveCursorPos)(float x, float y, float z);
 
-	// Local Axis Management functions
+	/* Local Axis Management functions */
 	void		(*setLocalAxis)(float forward[3]);
 	void		(*setLocalAxisFromLine)(float startX, float startY, float startZ, float endX, float endY, float endZ);
 	void		(*setLocalAxisFromCoords)(coords_t start, coords_t end);
 
-	// Utility functions
+	/* Utility functions */
 	float		(*getDistanceBetweenVertices)(vertex_t *vert1, vertex_t *vert2);
 	void		(*resetWorkSpace)(void);
 
-	// Rendering functions
+	/* Rendering functions */
 	void		(*updateMeshGeometry)(mesh_t *mesh);
 	void		(*renderMesh)(const mesh_t *mesh, float viewMatrix[16]);
 } _geometry_functions;
